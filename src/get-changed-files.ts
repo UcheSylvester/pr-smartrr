a6bed0abf8f7594071e7733c1415bd7369457234
UcheSylvester <okorocode@gmail.com>
2023-11-06 00:45:35 +0100

    chores: minor changes

--- src/get-changed-files.ts
+++ src/get-changed-files.ts
@@ -1,8 +1,10 @@
 import { exec } from 'child_process';
 
 export const getChangedFiles = async (base: string, head: string) => {
+  const command = `git diff --name-only ${base} ${head}`;
+  console.log({ command, base, head });
   return new Promise<string>((resolve, reject) => {
-    exec(`git diff --name-only ${base} ${head}`, (error, stdout) => {
+    exec(command, (error, stdout) => {
       if (error) {
         reject(error.message);
       }
