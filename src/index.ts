71c867b0d68417a9de4774aedb92182169028538
UcheSylvester <okorocode@gmail.com>
2023-11-05 23:18:30 +0100

    chores: add default main workflow

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello word!!!');
+import { context } from '@actions/github';
+import { getChangedFiles } from './helpers/get-changed-files';
+
+/**
+ * STEPS
+ * V0.0.1
+ * Get PR creator
+ * Get changed files
+ * Get committers of the changed files
+ * Compare committers and take the top 3 committers (excluding the creator) with the most commits as reviewers
+ * Get reviewers emails
+ * Get github username from emails
+ * Add reviewers to PR
+ *
+ * V0.0.2
+ * Allow for default reviewers incase there are no reviewers found (e.g. for new files)
+ *
+ */
+const run = async () => {
+  const creator = context.payload.pull_request?.user?.login;
+
+  const changedFiles = getChangedFiles();
+
+  console.log({ creator, changedFiles });
+};
+
+run();
